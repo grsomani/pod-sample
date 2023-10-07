8ca770174f126d9396051c3d5e0f786e7e46a3c3
Ganesh Somani <ganesh.somani@gmail.com>
2023-10-07 12:32:55 +0530

    Added GreetAgent

---
+++ first-framework/GreetAgent.swift
@@ -0,0 +1,14 @@
+//
+//  GreetAgent.swift
+//  first-framework
+//
+//  Created by Ganesh Somani on 07/10/23.
+//
+
+import Foundation
+
+public class GreetAgent {
+    public func greetUser(name: String) -> String {
+        return "Hello \(name)"
+    }
+}
