a499cab3c78fb91904a86111625f80a897f9e36d
Ganesh Somani <ganesh.somani@gmail.com>
2023-10-07 12:28:47 +0530

    Initial Commit

---
+++ first-framework/first_framework.h
@@ -0,0 +1,18 @@
+//
+//  first_framework.h
+//  first-framework
+//
+//  Created by Ganesh Somani on 07/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for first_framework.
+FOUNDATION_EXPORT double first_frameworkVersionNumber;
+
+//! Project version string for first_framework.
+FOUNDATION_EXPORT const unsigned char first_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <first_framework/PublicHeader.h>
+
+
